5421f753854e72cc41978ac91094efdb64d74cd5
sumitsdubey <sumitstudyguide@gmail.com>
2025-07-25 18:47:29 +0530

    deployed Url updated

--- src/app/config.js
+++ src/app/config.js
@@ -1,3 +1,3 @@
-export const baseUrl = "http://localhost:8080/api/v1";
+export const baseUrl = "https://aasanorder-backend.onrender.com/api/v1";
 
 export const imageApi = "https://www.themealdb.com/api/json/v1/1/search.php?s="
